1a8f144f4a08364cd3db1334aea6fe7493ff246c
Surajit <85634769+surajit20107@users.noreply.github.com>
2025-08-31 13:43:12 +0000

    Login & register implemented

--- models/joke.model.go
+++ models/joke.model.go
@@ -1,16 +1,12 @@
 package models
 
 import (
-	//"time"
 	"gorm.io/gorm"
 )
 
 type Joke struct {
 	gorm.Model
-	// Id        uint      `gorm:"primaryKey" json:"id"`
-	Content   string    `json:"content" binding:"required"`
-	Author    uint    `json:"author" binding:"required"`
-	
-	// CreatedAt time.Time `json:"created_at"`
-	// UpdatedAt time.Time `json:"updated_at"`
+	Content string `json:"content" binding:"required"`
+	Author  uint   `json:"author"`
+	User    User   `gorm:"foreignKey:Author;constraint:OnDelete:CASCADE;"`
 }
--- models/user.model.go
+++ models/user.model.go
@@ -1,17 +1,13 @@
 package models
 
 import (
-	//"time"
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
-	//ID        uint   `gorm:"primaryKey"`
-	Name      string `gorm:"not null" json:"name" binding:"required,min=5,max=30"`
-	Email     string `gorm:"unique;not null" json:"email" binding:"required,email,min=8,max=30"`
-	Password  string `gorm:"not null" json:"password" binding:"required,min=8,max=20"`
-	Jokes []Joke `gorm:"foreignKey:Author"`
-	//CreatedAt time.Time
-	//UpdatedAt time.Time
+	Name     string `gorm:"not null" json:"name" binding:"required,min=5,max=30"`
+	Email    string `gorm:"unique;not null" json:"email" binding:"required,email,min=8,max=30"`
+	Password string `gorm:"not null" json:"password" binding:"required,min=8,max=20"`
+	Jokes    []Joke `gorm:"foreignKey:Author"`
 }
